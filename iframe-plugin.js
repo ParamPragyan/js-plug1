ff115e780f5ec20dd36e3cada3a140e9f8728bd9
Param Pragyan <54448525+ParamPragyan@users.noreply.github.com>
2023-05-10 19:02:40 +0530

    Update iframe-plugin.js

--- iframe-plugin.js
+++ iframe-plugin.js
@@ -1,18 +1,4 @@
-// (function () {
-//   var urlParams = new URLSearchParams(window.location.search);
-//   var iframeUrl = urlParams.get("iframe_url");
 
-//   if (iframeUrl) {
-//     var iframe = document.createElement("iframe");
-//     iframe.src = iframeUrl;
-//     iframe.width = "100%";
-//     iframe.height = "500px";
-//     console.log(document.querySelector(".abc"));
-//     console.log(iframe)
-
-//     document.body.appendChild(iframe);
-//   }
-// })();
 (function () {
   var urlParams = new URLSearchParams(window.location.search);
   var iframeUrl = urlParams.get("iframe_url");
@@ -20,13 +6,10 @@
   if (iframeUrl) {
     var iframe = document.createElement("iframe");
     iframe.src = iframeUrl;
-    iframe.style.width = "100%";
-    iframe.style.height = "500px";
+    iframe.width = "500px";
+    iframe.height = "500px";
     document.body.appendChild(iframe);
   }
 })();
 
-// var urlParams = new URLSearchParams(window.location.search);
-// var iframeUrl = urlParams.get("iframe_url");
-// console.log(iframeUrl);
-// console.log(document.querySelector(".abc"));
+
